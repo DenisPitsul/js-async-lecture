d54691d383b8f8f9e10d50453214050f8bd2c8b5
DenisPitsul <denia.pitsul@gmail.com>
2024-06-23 19:25:00 +0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// const showAlert = () => {
+//     alert('Hello');
+// }
+
+// const timeoutId = setTimeout(showAlert, 2000);
+// // clearInterval(timeoutId);
+
+// const printToConsole = () => {
+//     console.log('to console');
+// }
+
+// const inervalId = setInterval(printToConsole, 1000);
+// // clearInterval(inervalId);
+
+// const offerLink = document.querySelector('.specOffer');
+
+// const removeOffer = () => {
+//     offerLink.remove();
+// }
+
+// setTimeout(removeOffer, 2000);
+
+// setTimeout(() => {
+
+// }, 3000);
+
+// const bodyEl = document.body;
+// const cookiesEl = document.querySelector('.cookies');
+// const okBtn = document.querySelector('.ok-btn');
+// cookiesEl.style.display = 'none';
+
+// const showCookies = () => {
+//     cookiesEl.style.display = 'block';
+//     okBtn.addEventListener('click', () => {
+//         cookiesEl.style.display = 'none';
+//     });
+// }
+// setTimeout(showCookies, 3000);
+
+// let timer = 1;
+// const timerEl = document.querySelector('.timer');
+
+// const updateTimer = () => {
+//     timer++;
+//     timerEl.textContent = timer;
+// };
+
+// setInterval(updateTimer, 1000);
